584237ad5d6d22868d1bd178265ea3efe07843bc
sumitshinde-84 <shindesumit217@gmail.com>
2023-06-23 19:43:53 +0530

    add IconComponent

---
+++ src/components/IconComponent.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="IconComponent" >
+    <div 
+      class="
+       flex
+       items-center
+       justify-center
+       cursor-pointer
+       rounded-full
+       w-10
+       h-10
+    "
+    :class="[hoverColor]"
+    :data-tooltip-target="`tooltip-no-arrow-${iconString}`"
+     data-tooltip-placement="bottom"
+    >
+    <component :is="icon" :size="iconSize" :fillColor="iconColor"/>
+    </div>
+    <div 
+    :id="`tooltip-no-arrow-${iconString}`" 
+    role="tooltip" 
+      class="
+        inline-block 
+        absolute 
+        invisible 
+        text-xs 
+        z-10 
+        py-1 
+        px-2 
+        font-medium 
+        text-white 
+        rounded-sm 
+        shadow-sm 
+        opacity-0 
+        tooltip 
+        bg-gray-600
+        delay-150
+      ">
+    {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { toRefs,defineProps } from 'vue';
+import MenuIcon from 'vue-material-design-icons/Menu.vue'
+import MagnifyIcon from 'vue-material-design-icons/Magnify.vue'
+import HelpIcon from 'vue-material-design-icons/Help.vue'
+import TuneIcon from 'vue-material-design-icons/Tune.vue'
+import CogOutlineIcon from 'vue-material-design-icons/CogOutline.vue'
+import AppsIcon from 'vue-material-design-icons/Apps.vue'
+import ArrowLeftIcon from 'vue-material-design-icons/ArrowLeft.vue'
+import TrashCanOutlineIcon from 'vue-material-design-icons/TrashCanOutline.vue'
+
+
+const props = defineProps({
+  iconString:String,
+  iconColor:String,
+  iconSize:Number,
+  text:String,
+  hoverColor:String
+})
+let icon = null
+
+const { iconString , iconColor , text ,hoverColor} = toRefs(props)
+
+if(iconString.value==='menu'){
+  icon=MenuIcon
+}
+
+if(iconString.value==='magnify'){
+  icon=MagnifyIcon
+}
+
+if(iconString.value==='help'){
+  icon=HelpIcon
+}
+
+if(iconString.value==='apps'){
+  icon=AppsIcon
+}
+
+if(iconString.value==='back'){
+  icon=ArrowLeftIcon
+}
+
+if(iconString.value==='cog'){
+  icon=CogOutlineIcon
+}
+
+if(iconString.value==='trash'){
+  icon=TrashCanOutlineIcon
+}
+
+if(iconString.value==='tune'){
+  icon=TuneIcon
+}
+</script>
+
+
