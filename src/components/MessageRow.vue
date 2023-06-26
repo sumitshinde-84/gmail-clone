0ecb8790040d144a2f7fddd501cf1f7493336b30
sumitshinde-84 <shindesumit217@gmail.com>
2023-06-26 19:34:34 +0530

    add message board and Message row

---
+++ src/components/MessageRow.vue
@@ -0,0 +1,89 @@
+<template>
+<div id="MessageRow">
+    <div class="
+              border-b
+              hover:border-gray-200
+              hover:border-y-2
+              hover-border-x-2
+              cursor-pointer">
+        <div class="flex items-center px-4 py-2">
+            <div class="flex items-center">
+                <component :is="CheckboxBlankOutline" :size="19" filcolor="#636363" />
+
+                <StarOutlineIcon :size="19" filcolor="#636363" class="ml-4" />
+            </div>
+            <div class="flex items-center w-full">
+                <router-link to="email/message" class="w-full">
+                    <div class="flex items-center justify-between">
+                        <div class="
+                              flex 
+                              items-centers
+                              w-full
+                            ">
+                            <div class="text-sm ml-4 font-semibold truncate-from">
+                                {{from}}
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <div class="text-sm mr-1.5 font-semibold  truncate-subject">
+                                        {{subject}}
+                                    </div>
+                                    <div class="text-sm mr-1.5 text-gray-500 font-semibold truncate-body">
+                                       - {{body}}...
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="text-right truncate w-full mr-4 text-xs font-semibold">{{ time }}</div>
+                    </div>
+
+                </router-link>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup>
+import {
+    toRefs,
+    defineProps
+} from 'vue';
+import CheckboxBlankOutline from 'vue-material-design-icons/CheckboxBlankOutline.vue'
+import StarOutlineIcon from 'vue-material-design-icons/StarOutline.vue'
+
+const props = defineProps({
+    from: String,
+    subject: String,
+    body: String,
+    time:String,
+})
+// let icon = null
+
+const {
+    from,
+    subject,
+    body
+} = toRefs(props)
+</script>
+<style lang="scss">
+
+.truncate-from{
+    width:170px;
+    text-overflow:ellipsis;
+    overflow:hidden;
+    white-space:nowrap;
+}
+
+.truncate-subject{
+    max-width:200px;
+    text-overflow:ellipsis;
+    overflow:hidden;
+    white-space:nowrap;
+}
+
+.truncate-body{
+    max-width:400px;
+    text-overflow:ellipsis;
+    overflow:hidden;
+    white-space:nowrap;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="home">
-    
-  </div>
+  <div id="HomeView" class="w-full bg-white ml-2 rounded-t-xl h-full shadow-sm">
+    <div class="border-b">
+      <div class="flex items-center justify-between px-4 my-3">
+        <IconComponent 
+            class="m-2 ml-2.5"
+            iconString="trash"
+            iconColor="#636363"
+            :iconSize="19"
+            text="Google apps"
+            hoverColor="hover:bg-gray-200"
+          />
+          <div class="text-xs text-gray-500">1-50 of 153</div>
+      </div>
+    </div>
+    <div>
+      <MessageRow
+         from="ronel@gamil.com"
+         subject="test row 1"
+         body="this is the body text"
+         time="Jun 20 15:15"
+      />
+    </div>
+</div>
 </template>
 
-<script>
+<script setup>
+  import IconComponent from '@/components/IconComponent.vue'
+  import MessageRow from "@/components/MessageRow.vue"
 
 </script>
